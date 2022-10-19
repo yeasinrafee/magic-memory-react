2a40a1b79b61844c903bbc5e3d6aa01a2e9f007d
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-10-20 02:07:02 +0600

    Adding a matched property into the card

--- magic-memory/src/App.js
+++ magic-memory/src/App.js
@@ -3,12 +3,12 @@ import './App.css'
 import SingleCard from './components/SingleCard';
 
 const cardImages = [
-  { "src": "/img/helmet-1.png" },
-  { "src": "/img/potion-1.png" },
-  { "src": "/img/ring-1.png" },
-  { "src": "/img/scroll-1.png" },
-  { "src": "/img/shield-1.png" },
-  { "src": "/img/sword-1.png" },
+  { "src": "/img/helmet-1.png", matched: false },
+  { "src": "/img/potion-1.png", matched: false },
+  { "src": "/img/ring-1.png", matched: false },
+  { "src": "/img/scroll-1.png", matched: false },
+  { "src": "/img/shield-1.png", matched: false },
+  { "src": "/img/sword-1.png", matched: false },
 ]
 
 function App() {
@@ -34,10 +34,18 @@ function App() {
   useEffect(() =>{
     if(choiceOne && choiceTwo){
       if(choiceOne.src === choiceTwo.src){
-        console.log("choice matched");
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if(card.src === choiceOne.src){
+              return {...card, matched: true}
+            }else{
+              return card;
+            }
+          })
+        })
         resetTurn();
       }else{
-        console.log("choice does not match");
+      
         resetTurn();
       }
     }
